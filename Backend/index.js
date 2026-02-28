8dd3607f4f0d571be986f46adca1d5cd097c3aad
natthawadi.pi <natthawadi.pi@ku.th>
2026-02-28 11:44:25 +0700

    add post ,get,delete

--- Backend/index.js
+++ Backend/index.js
@@ -127,3 +127,84 @@ app.listen(port, async () => {
     await initDBConnection()
     console.log(`Server is running on port ${port}`)
 })
+app.post('/users',async (req,res) => {
+    try{
+        let user = req.body
+        const results = await conn.query('INSERT INTO users SET ?',user)
+        res.json({
+            message: 'User created successfully',
+            data: results[0]
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Database insert error'});
+        res.status(500).json({
+            message:'Error creating user',
+            error: error.message
+        });
+    }
+});
+//สำหรับดึงข้อมูลผู้ใช้จากฐานข้อมูลตาม id ที่ส่งมาใน query string
+app.get('/users',async (req,res) => {
+    try{
+        let id=req.query.id
+        const results = await conn.query('SELECT * FROM users WHERE id = ?', id)
+        if(results[0].length === 0){
+            throw{statusCode:404, message:'User not found'};
+
+            }
+            res.json(results[0][0])
+    }
+    catch(error){
+        console.error('Error fetching user:', error.message);
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message:'Error fetching user',
+            error: error.message
+        });
+    }
+});
+//PUT /users/:id สำหรับแก้ไขข้อมูล user ที่มี id ตรงกับที่ส่งมาใน URL
+app.put('/users/:id', async(req,res)=>{
+    try{
+        let id=req.params.id
+        let updateUser=req.body;
+        const results=await conn.query('UPDATE users SET ? WHERE id = ?', [updateUser, id])
+        if(results[0].affectedRows==0){
+            throw{statusCode:404, message:'User not found'};
+        }
+        res.json({
+            message:'User updated successfully',
+            data:updateUser
+        });
+    }
+    catch(error){
+        console.error('Error updating user:', error.message);
+        let statusCode=error.statusCode||500;
+        res.status(statusCode).json({
+            message:'Error updating user',
+            error: error.message
+        });
+    }
+})
+//DELETE /users/:id สำหรับลบ user ที่มี id ตรงกับที่ส่งมาใน URL
+app.delete('/users/:id', async(req,res)=>{
+    try{
+        let id=req.pqrqms.id
+        const results=await conn.query('DELETE FROM users WHERE id = ?',id)
+        if(results[0].affectedRows==0){
+            throw{statusCode:404,message:'User not found'};
+        }
+        res.json({
+            message:'User deleted successfully'
+        });
+    }
+    catch(error){
+        console.error('Error deleting user:',error.message);
+        let statusCode=error.statusCode||500;
+        res.status(statusCode).json({
+            message:'Error deleting user',
+            error:error.message
+        });  
+    }
+})
